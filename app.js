bce011bceecd5539961aa76c2d26e6c29ac17c64
Yldemaro R. <yldemaro.1994@gmail.com>
2025-07-03 14:37:54 -0400

    feat: simple routes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import http from 'http';
+import routes from './routes.js';
 
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(3000, () => {
+const server = http.createServer(routes)
+
+server.listen(3000, () => {
   console.log('Server running at http://localhost:3000/');
 }); 
