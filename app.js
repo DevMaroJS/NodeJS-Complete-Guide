d8eccfb41a2e999050279e6fb487f9951c66e6e1
Yldemaro R. <yldemaro.1994@gmail.com>
2025-07-08 16:10:36 -0400

    refactor: Extract routing to routes.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import http from 'http';
-import routes from './routes.js';
+import http from "http";
+import routes from "./routes.js";
 
-const server = http.createServer(routes)
+const server = http.createServer(routes);
 
 server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
-}); 
+  console.log("Server running at http://localhost:3000/");
+});
