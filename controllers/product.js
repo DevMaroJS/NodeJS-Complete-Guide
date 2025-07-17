9f07b701cbfa78aef3b994699a003c1ddafac706
Yldemaro R. <yldemaro.1994@gmail.com>
2025-07-17 15:55:31 -0400

    feat: improve product logic

--- controllers/product.js
+++ controllers/product.js
@@ -1,38 +1,59 @@
 const products = [];
 
 export const createProduct = (req, res) => {
-  const product = req.body;
+  const body = req.body;
+  const newProduct = {
+    ...body,
+    id: Math.random().toString(),
+  };
+  products.push(newProduct);
 
-  products.push({
-    ...product,
-    id: products.length,
-  });
-
-  return res.json({ message: "Product created" });
+  return res.json({ message: "Product created", data: newProduct });
 };
 
 export const getProducts = (req, res) => {
-  return res.json(products);
+  return res.json({ data: products });
 };
 
 export const updateProduct = (req, res) => {
   const id = req.params.id;
-  const product = req.body;
-  products[id] = {
-    ...products[id],
-    ...product,
+  const body = req.body;
+
+  const productIndex = products.findIndex((product) => product.id === id);
+
+  if (productIndex === -1) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  const productUpdated = {
+    ...products[productIndex],
+    ...body,
   };
-  return res.json({ message: "Product updated" });
+
+  products[productIndex] = productUpdated;
+
+  return res.json({ message: "Product updated", data: productUpdated });
 };
 
 export const deleteProduct = (req, res) => {
   const id = req.params.id;
-  products.splice(id, 1);
+
+  const productIndex = products.findIndex((product) => product.id === id);
+
+  if (productIndex === -1) {
+    return res.status(404).json({ error: "Product not found" });
+  }
+
+  products.splice(productIndex, 1);
   return res.json({ message: "Product deleted" });
 };
 
 export const getProduct = (req, res) => {
   const id = req.params.id;
-  const product = products[id];
+  const product = products.find((product) => product.id === id);
+
+  if (!product) {
+    return res.status(404).json({ error: "Product not found" });
+  }
   return res.json(product);
 };
